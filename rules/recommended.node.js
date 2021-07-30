6edd7fb71f066bf63eeaa01a19ee4453e40d68d7
Andrii Shkodiak <andrii.shkodiak@rentware.io>
2021-07-30 18:57:45 +0200

    Changed prefix for local imports

--- rules/recommended.node.js
+++ rules/recommended.node.js
@@ -66,7 +66,7 @@ module.exports = {
         'index',
       ],
       pathGroups: [
-        {pattern: '~*', group: 'external'},
+        {pattern: '#*', group: 'external'},
       ],
     },
   ],
