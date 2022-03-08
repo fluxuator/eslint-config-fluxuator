819a0eb081680d5ffd0781210ef417afb148f3e4
Andrii Shkodiak <andrii.shkodiak@rentware.io>
2022-03-08 20:34:32 +0100

    chore(commitlint): adjusted ``@commitlint/config-conventional` rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': [1, 'always', Infinity],
     'body-max-line-length': [1, 'always', Infinity],
     'footer-max-line-length': [1, 'always', Infinity],
+    'subject-case': [1, 'never'],
   },
 }
