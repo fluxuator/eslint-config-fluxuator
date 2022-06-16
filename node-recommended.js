fda512810302a6eaa9f3c27adbb6861ec611458e
Andrii Shkodiak <andrii.shkodiak@rentware.io>
2022-06-16 17:21:17 +0200

    feat(node): enabled TypeScript support for NodeJS linters

--- node-recommended.js
+++ node-recommended.js
@@ -5,4 +5,10 @@
  */
 module.exports = {
   rules: require('./rules/recommended.node'),
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: require('./rules/recommended.typescript'),
+    },
+  ],
 }
--- node.js
+++ node.js
@@ -20,5 +20,25 @@ module.exports = {
     sourceType: 'module',
   },
 
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      // If adding a typescript-eslint version of an existing ESLint rule,
+      // make sure to disable the ESLint rule here.
+      rules: require('./rules/typescript'),
+    },
+  ],
+
   rules: require('./rules/node'),
 }
