c7f938b077913f6dfcfc4d56babf8213b586e9a7
Andrii Shkodiak <fluxuator@gmail.com>
2026-08-03 00:40:39 +0200

    feat(plugins): port restrict-import-paths rule from fareamt
    Ported from ~/Projects/Flix/eslint-config-fareamt's
    lib/plugins/rules/restrict-import-paths.*, no logic changes. Doc
    updated to reference the renamed fluxuator/restrict-import-paths rule
    ID.

---
+++ plugins/rules/restrict-import-paths.rule.js
@@ -0,0 +1,170 @@
+const { getPathsDepthDiff, getRelativePath } = require('../utils/path.utils')
+const path = require('path')
+const {
+  resolveTsConfigPath,
+  getTsConfig,
+  isPathAlias,
+  resolvePathByAlias,
+  resolveAliasByPath,
+  normalizeAliasPaths,
+} = require('../utils/typescript.utils')
+
+/** @type {import("eslint").Rule.RuleModule} */
+module.exports = {
+  meta: {
+    type: 'layout',
+    fixable: 'code',
+    messages: {
+      preferRelative: 'Use a relative import path "{{path}}" instead of alias',
+      preferAlias: 'Use an aliased import path "{{path}}" instead of a relative one',
+    },
+    schema: {
+      type: 'array',
+      minItems: 0,
+      maxItems: 1,
+      items: [
+        {
+          type: 'object',
+          properties: {
+            useTsConfig: {
+              anyOf: [
+                {
+                  type: 'boolean',
+                },
+                {
+                  type: 'string',
+                },
+              ],
+            },
+            allowedPathDepth: { type: 'number' },
+            rootDir: { type: 'string' },
+          },
+          additionalProperties: false,
+        },
+      ],
+    },
+  },
+
+  /**
+   * @param {import("eslint").Rule.RuleContext} context
+   *
+   * @returns {import("eslint").Rule.NodeListener}
+   */
+  create(context) {
+    const {
+      /**
+       * -1 - any relation path is allowed
+       * 0 - relation path within the same directory is allowed
+       * 1 - relation path to parent directory is allowed
+       * 2 - relation path to grandparent directory is allowed
+       * ...
+       */
+      allowedPathDepth = -1,
+      rootDir = './src',
+      useTsConfig = true,
+    } = context.options[0] || {}
+    const tsConfigPath = resolveTsConfigPath(context.cwd, useTsConfig)
+    const workingDirPath = tsConfigPath ? path.dirname(tsConfigPath) : context.cwd
+    const rootDirPath = path.resolve(workingDirPath, rootDir)
+    let aliasedPaths
+
+    if (tsConfigPath) {
+      const tsConfig = getTsConfig(tsConfigPath)
+
+      aliasedPaths = tsConfig ? normalizeAliasPaths(tsConfig.basePath, tsConfig.paths) : undefined
+    }
+
+    /**
+     * Gets the top-level directory within the root directory
+     * @param {string} filePath - Absolute file path
+     * @param {string} rootPath - Absolute root path
+     * @returns {string} The top-level directory name
+     */
+    function getTopLevelDir(filePath, rootPath) {
+      const relativePath = path.relative(rootPath, filePath)
+      const parts = relativePath.split(path.sep)
+
+      return parts[0]
+    }
+
+    return {
+      ImportDeclaration(node) {
+        if (!aliasedPaths) {
+          // No aliases - nothing to check
+          return
+        }
+
+        const currentFilePath = context.filename
+        const contextDirPath = path.dirname(currentFilePath)
+
+        if (!currentFilePath.startsWith(rootDirPath)) {
+          // Skip files that are not in the root directory
+
+          return
+        }
+
+        const importPath = node.source.value
+        const isRelativePath = importPath.startsWith('.')
+        const isAliasedPath = isPathAlias(importPath, aliasedPaths)
+
+        if (!isRelativePath && !isAliasedPath) {
+          // We're not interested in import paths of vendors
+          return
+        }
+
+        let resolvedImportedPath
+
+        if (isRelativePath) {
+          resolvedImportedPath = path.resolve(contextDirPath, importPath)
+        } else if (isAliasedPath) {
+          const resolvedAliasedPath = resolvePathByAlias(importPath, aliasedPaths)
+
+          resolvedImportedPath = path.resolve(workingDirPath, resolvedAliasedPath)
+        } else {
+          throw new Error('Unexpected path type')
+        }
+
+        const importPathDepthDiff = getPathsDepthDiff(currentFilePath, resolvedImportedPath)
+
+        // Check if both files are in the same top-level module directory
+        const currentTopLevelDir = getTopLevelDir(currentFilePath, rootDirPath)
+        const importedTopLevelDir = getTopLevelDir(resolvedImportedPath, rootDirPath)
+        const isSameModule = currentTopLevelDir === importedTopLevelDir
+
+        if (isAliasedPath && importPathDepthDiff <= allowedPathDepth && isSameModule) {
+          const relativePath = getRelativePath(currentFilePath, resolvedImportedPath)
+
+          context.report({
+            node,
+            messageId: 'preferRelative',
+            data: {
+              path: relativePath,
+            },
+            fix(fixer) {
+              return fixer.replaceTextRange([node.source.range[0] + 1, node.source.range[1] - 1], relativePath)
+            },
+          })
+        }
+
+        if (isRelativePath && (importPathDepthDiff > allowedPathDepth || !isSameModule)) {
+          const aliasedPath = resolveAliasByPath(workingDirPath, resolvedImportedPath, aliasedPaths)
+
+          if (!aliasedPath) {
+            return
+          }
+
+          context.report({
+            node,
+            messageId: 'preferAlias',
+            data: {
+              path: aliasedPath,
+            },
+            fix(fixer) {
+              return fixer.replaceTextRange([node.source.range[0] + 1, node.source.range[1] - 1], aliasedPath)
+            },
+          })
+        }
+      },
+    }
+  },
+}
---
+++ plugins/rules/restrict-import-paths.rule.spec.js
@@ -0,0 +1,279 @@
+const { RuleTester } = require('eslint')
+const restrictImportPathsRule = require('./restrict-import-paths.rule')
+const path = require('path')
+
+/** @type {import("eslint").RuleTester} */
+const ruleTester = new RuleTester({
+  languageOptions: { ecmaVersion: 2015 },
+})
+
+const basePath = path.resolve(__dirname, '../__fixtures__')
+
+const defaultConfigCtx = {
+  filename: path.join(basePath, 'src/app/dir1/file.ts'),
+  options: [
+    {
+      useTsConfig: path.join(basePath, 'tsconfig.json'),
+    },
+  ],
+}
+
+ruleTester.run('restrict-import-paths: default config', restrictImportPathsRule, {
+  valid: [
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from '#app/foo';",
+    },
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from '#app/file.ts';",
+    },
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from '#app/dir2/file.ts';",
+    },
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from 'ext-lib';",
+    },
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from '#unknown-alias';",
+    },
+  ],
+  invalid: [
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from '../foo';",
+      output: "import { foo } from '#app/foo';",
+      errors: [{ message: 'Use an aliased import path "#app/foo" instead of a relative one' }],
+    },
+    {
+      ...defaultConfigCtx,
+      code: "import { foo } from './file.spec.ts';",
+      output: "import { foo } from '#app/dir1/file.spec.ts';",
+      errors: [{ message: 'Use an aliased import path "#app/dir1/file.spec.ts" instead of a relative one' }],
+    },
+  ],
+})
+
+const sameFolderConfigCtx = {
+  filename: path.join(basePath, 'src/app/dir1/file.ts'),
+  options: [
+    {
+      allowedPathDepth: 0,
+      useTsConfig: path.join(basePath, 'tsconfig.json'),
+    },
+  ],
+}
+
+ruleTester.run(
+  'restrict-import-paths: default config but allowed releative path within same folder',
+  restrictImportPathsRule,
+  {
+    valid: [
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from './foo';",
+      },
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from '#app/file.ts';",
+      },
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from '#app/dir2/file.ts';",
+      },
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from 'ext-lib';",
+      },
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from '#unknown-alias';",
+      },
+    ],
+    invalid: [
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from '../foo';",
+        output: "import { foo } from '#app/foo';",
+        errors: [{ message: 'Use an aliased import path "#app/foo" instead of a relative one' }],
+      },
+      {
+        ...sameFolderConfigCtx,
+        code: "import { foo } from '#app/dir1/file.spec.ts';",
+        output: "import { foo } from './file.spec.ts';",
+        errors: [{ message: 'Use a relative import path "./file.spec.ts" instead of alias' }],
+      },
+    ],
+  }
+)
+
+const anotherConfigCtx = {
+  filename: path.join(basePath, 'app/dir1/file.ts'),
+  options: [
+    {
+      allowedPathDepth: 0,
+      rootDir: './app',
+      useTsConfig: path.join(basePath, 'tsconfig.another.json'),
+    },
+  ],
+}
+
+ruleTester.run('restrict-import-paths: another config', restrictImportPathsRule, {
+  valid: [
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from './foo';",
+    },
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from '#app/file.ts';",
+    },
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from '#app/dir2/file.ts';",
+    },
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from 'ext-lib';",
+    },
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from '#unknown-alias';",
+    },
+  ],
+  invalid: [
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from '../foo';",
+      output: "import { foo } from '#app/foo';",
+      errors: [{ message: 'Use an aliased import path "#app/foo" instead of a relative one' }],
+    },
+    {
+      ...anotherConfigCtx,
+      code: "import { foo } from '#app/dir1/file.spec.ts';",
+      output: "import { foo } from './file.spec.ts';",
+      errors: [{ message: 'Use a relative import path "./file.spec.ts" instead of alias' }],
+    },
+  ],
+})
+
+const simpleConfigCtxDepth0 = {
+  filename: path.join(basePath, 'src/dir1/file.ts'),
+  options: [
+    {
+      allowedPathDepth: 0,
+      rootDir: './src',
+      useTsConfig: path.join(basePath, 'tsconfig.simple.json'),
+    },
+  ],
+}
+
+ruleTester.run('restrict-import-paths: simple config depth 0', restrictImportPathsRule, {
+  valid: [
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from './foo';",
+    },
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from '#app/file.ts';",
+    },
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from '#app/dir2/file.ts';",
+    },
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from 'ext-lib';",
+    },
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from '#unknown-alias';",
+    },
+  ],
+  invalid: [
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from '../foo';",
+      output: "import { foo } from '#foo';",
+      errors: [{ message: 'Use an aliased import path "#foo" instead of a relative one' }],
+    },
+    {
+      ...simpleConfigCtxDepth0,
+      code: "import { foo } from '#dir1/file.spec.ts';",
+      output: "import { foo } from './file.spec.ts';",
+      errors: [{ message: 'Use a relative import path "./file.spec.ts" instead of alias' }],
+    },
+  ],
+})
+
+const simpleConfigCtxDepth1 = {
+  options: [
+    {
+      allowedPathDepth: 1,
+      rootDir: './src',
+      useTsConfig: path.join(basePath, 'tsconfig.simple.json'),
+    },
+  ],
+}
+
+ruleTester.run('restrict-import-paths: simple config depth 1', restrictImportPathsRule, {
+  valid: [
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from './foo';",
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from '#app/file.ts';",
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from '#app/dir1/file.ts';",
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/sub-dir1/file.ts'),
+      code: "import { foo } from '../sub-dir2/file.ts';",
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from 'ext-lib';",
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from '#unknown-alias';",
+    },
+  ],
+  invalid: [
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from '../foo';",
+      output: "import { foo } from '#foo';",
+      errors: [{ message: 'Use an aliased import path "#foo" instead of a relative one' }],
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/file.ts'),
+      code: "import { foo } from '#dir1/file.spec.ts';",
+      output: "import { foo } from './file.spec.ts';",
+      errors: [{ message: 'Use a relative import path "./file.spec.ts" instead of alias' }],
+    },
+    {
+      ...simpleConfigCtxDepth1,
+      filename: path.join(basePath, 'src/dir1/sub-dir1/file.ts'),
+      code: "import { foo } from '#dir1/sub-dir2/file.ts';",
+      output: "import { foo } from '../sub-dir2/file.ts';",
+      errors: [{ message: 'Use a relative import path "../sub-dir2/file.ts" instead of alias' }],
+    },
+  ],
+})
